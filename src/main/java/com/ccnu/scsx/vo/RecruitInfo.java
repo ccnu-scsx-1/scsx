b4c14b10864d3297e826c4870193d0f1efa56919
Joban_Cai <Joban_Cai@163.com>
2017-01-04 14:48:42 +0800

    Test IntellIj

--- src/main/java/com/ccnu/scsx/vo/RecruitInfo.java
+++ src/main/java/com/ccnu/scsx/vo/RecruitInfo.java
@@ -5,5 +5,59 @@ package com.ccnu.scsx.vo;
  */
 public class RecruitInfo {
 
+  private String infoId;
+  private String companyId;
+  private String title;
+  private int salary_high;
+  private int salary_low;
+  private String region;
+
+  public String getInfoId() {
+    return infoId;
+  }
+
+  public void setInfoId(String infoId) {
+    this.infoId = infoId;
+  }
+
+  public String getCompanyId() {
+    return companyId;
+  }
+
+  public void setCompanyId(String companyId) {
+    this.companyId = companyId;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public int getSalary_high() {
+    return salary_high;
+  }
+
+  public void setSalary_high(int salary_high) {
+    this.salary_high = salary_high;
+  }
+
+  public int getSalary_low() {
+    return salary_low;
+  }
+
+  public void setSalary_low(int salary_low) {
+    this.salary_low = salary_low;
+  }
+
+  public String getRegion() {
+    return region;
+  }
+
+  public void setRegion(String region) {
+    this.region = region;
+  }
 }
 
